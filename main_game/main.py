f5f41d504bbaa80c49c2efbc8908d273e4f1f4ea
Stanford Lin <stanlin1999@gmail.com>
2020-06-28 10:14:22 -0700

    working on trinket

--- main_game/main.py
+++ main_game/main.py
@@ -16,7 +16,7 @@ screen_height = 600
 screen = pygame.display.set_mode()
 bg_color = (255, 255, 255)
 
-home_image = pygame.image.load("Foundation (d2) - Copy/57.jpg")
+home_image = pygame.image.load("57.jpg")
 screen = pygame.display.set_mode((screen_width, screen_height))
 FPS = 57
 i = 35
@@ -32,7 +32,7 @@ backgrounds_dooropen = ["Challenge Room1(Door1Open).jpg", "JupiterChallengeRoom(
 l = 0  # index for background_dooropen list (different from k)
 '''
 
-play = pygame.image.load("Buttons/Play-Button.png").convert_alpha()
+play = pygame.image.load("Play-Button.png").convert_alpha()
 play_button = pygame.Rect(325, 500, 150, 50)
 
 clock = pygame.time.Clock()
@@ -238,18 +238,18 @@ ball_velocity = 7
 gravity = 9.8
 lives = 3
 
-speech_1 = pygame.image.load("intro_text/IntroText(1).png")
-speech_2 = pygame.image.load("intro_text/IntroText(1).png")
-speech_3 = pygame.image.load("intro_text/IntroText(3).png")
-speech_4 = pygame.image.load("intro_text/IntroText(4).png")
-speech_5 = pygame.image.load("intro_text/IntroText(5).png")
-speech_6 = pygame.image.load("intro_text/IntroText(6).png")
+speech_1 = pygame.image.load("IntroText(1).png")
+speech_2 = pygame.image.load("IntroText(1).png")
+speech_3 = pygame.image.load("IntroText(3).png")
+speech_4 = pygame.image.load("IntroText(4).png")
+speech_5 = pygame.image.load("IntroText(5).png")
+speech_6 = pygame.image.load("IntroText(6).png")
 
-sound1 = pygame.mixer.Sound('voice_overs_wav/Part-1.wav')
-sound2 = pygame.mixer.Sound('voice_overs_wav/Part-2.wav')
-sound3 = pygame.mixer.Sound('voice_overs_wav/Part-3.wav')
-sound4 = pygame.mixer.Sound('voice_overs_wav/Part-4_1.wav')
-sound5 = pygame.mixer.Sound('voice_overs_wav/Part-5_1.wav')
+sound1 = pygame.mixer.Sound('Part-1.wav')
+sound2 = pygame.mixer.Sound('Part-2.wav')
+sound3 = pygame.mixer.Sound('Part-3.wav')
+sound4 = pygame.mixer.Sound('Part-4_1.wav')
+sound5 = pygame.mixer.Sound('Part-5_1.wav')
 
 
 
@@ -285,22 +285,21 @@ font = pygame.font.SysFont("comicsans", 30, True)
 '''
 
 
-def load_images(path):
+def load_images():
     images = []
-    imagelist = sorted(os.listdir(path))
     imagelist = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg',  '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', 
      '20.jpg', '21.jpg', '22.jpg', '23.jpg', '24.jpg', '25.jpg', '26.jpg', '27.jpg', '28.jpg', '29.jpg',  
     '30.jpg', '31.jpg', '32.jpg', '33.jpg', '34.jpg', '35.jpg', '36.jpg', '37.jpg', '38.jpg', '39.jpg', '40.jpg', 
     '41.jpg', '42.jpg', '43.jpg', '44.jpg', '45.jpg', '46.jpg', '47.jpg', '48.jpg', '49.jpg', '50.jpg', '51.jpg', 
     '52.jpg', '53.jpg', '54.jpg', '55.jpg', '56.jpg', '57.jpg']
     for file_name in imagelist:
-        image = pygame.image.load(path + os.sep + file_name).convert()
+        image = pygame.image.load(file_name).convert()
         images.append(image)
     return images
 
 
 def main():
-    images = load_images(path="Foundation (d2) - copy")  # Make sure to provide the relative or full path to the images directory.
+    images = load_images()  # Make sure to provide the relative or full path to the images directory.
     player = AnimatedSprite(position=(0, 0), images=images)
     all_sprites = pygame.sprite.Group(player)  # Creates a sprite group and adds 'player' to it.
 
